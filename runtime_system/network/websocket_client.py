bc5258533c4ac1d78dce19df45048e043fc42ef1
Cedrik-Meyer <cedrik.meyer02@gmail.com>
2026-06-08 14:54:24 +0200

    B3_02: WS Event-Handler

--- runtime_system/network/websocket_client.py
+++ runtime_system/network/websocket_client.py
@@ -1,5 +1,7 @@
 import asyncio
+import json
 import websockets
+
 
 class AgentWebSocketClient:
     def __init__(self, base_url: str, token: str):
@@ -7,6 +9,10 @@ class AgentWebSocketClient:
         self.token = token
         self.ws_url = f"{self.base_url}/ws/agent?token={self.token}"
         self.websocket = None
+        self.handlers = {}
+
+    def register_handler(self, msg_type: str, handler_func):
+        self.handlers[msg_type] = handler_func
 
     async def connect(self):
         self.websocket = await websockets.connect(self.ws_url)
@@ -15,3 +21,29 @@ class AgentWebSocketClient:
         if self.websocket is not None:
             await self.websocket.close()
             self.websocket = None
+
+    async def listen(self):
+        if self.websocket is None:
+            return
+
+        async for message in self.websocket:
+            try:
+                data = json.loads(message)
+                msg_type = data.get("type")
+
+                if msg_type in self.handlers:
+                    if asyncio.iscoroutinefunction(self.handlers[msg_type]):
+                        await self.handlers[msg_type](data)
+                    else:
+                        self.handlers[msg_type](data)
+                else:
+                    await self.handle_unregistered_message(data)
+            except json.JSONDecodeError:
+                pass
+
+    async def handle_unregistered_message(self, data: dict):
+        pass
+
+    async def send_message(self, message: dict):
+        if self.websocket is not None:
+            await self.websocket.send(json.dumps(message))
